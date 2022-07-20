293b7b5f453ab213576fb143dd91cad58b783f15
user2 <smart.ass.raty@gmail.com>
2022-07-20 12:03:57 +0600

    taskssch done

--- lib/planner.dart
+++ lib/planner.dart
@@ -26,105 +26,110 @@ class PlannerPageState extends State<PlannerPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(),
-        body: Center(
-            child: Form(
-                key: formkey,
-                child: Card(
-                  child: Column(
-                    children: <Widget>[
-                      TextFormField(
-                        decoration:
-                            const InputDecoration(hintText: "Disk to backup"),
-                        controller: diskController,
-                        validator: (value) {
-                          if (value == null || value.isEmpty) {
-                            return "Must be filled";
-                          }
-                          RegExp reg = RegExp("^[A-Z]:");
-                          if (!value.contains(reg)) {
-                            return "Usage: {[letter]:}";
-                          }
-                          return null;
-                        },
-                      ),
-                      TextFormField(
-                        decoration:
-                            const InputDecoration(hintText: "Save backup"),
-                        controller: backupController,
-                        validator: (value) {
-                          if (value == null || value.isEmpty) {
-                            return "Must be filled";
-                          }
-                          RegExp reg = RegExp("^[A-Z]:");
-                          if (!value.contains(reg)) {
-                            return "Usage: {[letter]:}";
-                          }
-                          return null;
-                        },
-                      ),
-                      TextFormField(
-                        decoration: const InputDecoration(
-                            hintText: "Days of week to backup"),
-                        controller: dayController,
-                        validator: (value) {
-                          if (value == null || value.isEmpty) {
-                            return "Must be filled";
-                          }
-                          RegExp reg = RegExp("^[A-Z]{3}");
-                          if (!value.contains(reg)) {
-                            return "Usage: {MON, TUE, WED, THU, FRI, SAT, SUN}";
-                          }
-                          return null;
-                        },
-                      ),
-                      Container(
-                          margin: const EdgeInsets.only(top: 10.0),
-                          child: ElevatedButton(
-                            onPressed: () {
-                              selectTime(context);
-                            },
-                            child: const Text("Set time"),
-                          )),
-                      const Spacer(),
-                      ElevatedButton(
-                          onPressed: () async {
-                            if (formkey.currentState!.validate()) {
-                              setState(() {
-                                diskToBackup = diskController.text;
-                                backupToDisk = backupController.text;
-                                dayToBackup = dayController.text;
-                                d = c.getNumberFromLetter(
-                                    backupToDisk.substring(0, 3));
-                              });
-                              showDialog(
-                                  context: context,
-                                  builder: (BuildContext context) {
-                                    return AlertDialog(
-                                      content: Text(
-                                          "[$diskToBackup], [$backupToDisk], [${backupToDisk.substring(0, 3)}], [$dayToBackup], [${d.serialNumber.toString()}]"),
-                                    );
-                                  });
-                              int a = checkSerial();
-                              if (a == 1) {
-                                var result = await Process.run(
-                                    'assets\\scripts\\Tasksch.bat', [],
-                                    runInShell: true);
-                              }
-                            } else {
-                              showDialog(
-                                  context: context,
-                                  builder: (BuildContext context) {
-                                    return const AlertDialog(
-                                      content: Text("Fail"),
-                                    );
-                                  });
-                            }
-                          },
-                          child: const Text("Confirm")),
-                    ],
+      appBar: AppBar(),
+      body: Center(
+        child: Form(
+          key: formkey,
+          child: Card(
+            child: Column(
+              children: <Widget>[
+                TextFormField(
+                  decoration: const InputDecoration(hintText: "Disk to backup"),
+                  controller: diskController,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return "Must be filled";
+                    }
+                    RegExp reg = RegExp("^[A-Z]:");
+                    if (!value.contains(reg)) {
+                      return "Usage: {[letter]:}";
+                    }
+                    return null;
+                  },
+                ),
+                TextFormField(
+                  decoration: const InputDecoration(hintText: "Save backup"),
+                  controller: backupController,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return "Must be filled";
+                    }
+                    RegExp reg = RegExp("^[A-Z]:");
+                    if (!value.contains(reg)) {
+                      return "Usage: {[letter]:}";
+                    }
+                    return null;
+                  },
+                ),
+                TextFormField(
+                  decoration: const InputDecoration(
+                  hintText: "Days of week to backup"),
+                  controller: dayController,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return "Must be filled";
+                    }
+                    RegExp reg = RegExp("^[A-Z]{3}");
+                    if (!value.contains(reg)) {
+                      return "Usage: {MON, TUE, WED, THU, FRI, SAT, SUN}";
+                    }
+                    return null;
+                  },
+                ),
+                Container(
+                  margin: const EdgeInsets.only(top: 10.0),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      selectTime(context);
+                    },
+                    child: const Text("Set time"),
+                    )
                   ),
-                ))));
+                const Spacer(),
+                ElevatedButton(
+                  onPressed: () async {
+                    if (formkey.currentState!.validate()) {
+                      setState(() {
+                        diskToBackup = diskController.text;
+                        backupToDisk = backupController.text;
+                        dayToBackup = dayController.text;
+                        d = c.getNumberFromLetter(backupToDisk.substring(0, 3));
+                      });
+                      showDialog(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return AlertDialog(
+                            content: Text("[$diskToBackup], [$backupToDisk], [${backupToDisk.substring(0, 3)}], [$dayToBackup], [${d.serialNumber.toString()}]"),
+                          );
+                        }
+                      );
+                      int a = checkSerial();
+                      if (a == 1) {
+                        writeFiles();
+                        //await Process.run('assets\\scripts\\Tasksch.bat', [], runInShell: true);
+                        //await Process.run('data\\flutter_assets\\assets\\scripts\\Tasksch.bat', [], runInShell: true);
+                      }
+                      //media();
+                    } 
+                    else {
+                      showDialog(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return const AlertDialog(
+                            content: Text("Fail"),
+                          );
+                        }
+                      );
+                    }
+                  },
+                  child: const Text("Confirm")
+                ),
+              ],
+            ),
+          )
+        )
+      )
+    );
   }
 
   selectTime(BuildContext context) async {
@@ -144,79 +149,65 @@ class PlannerPageState extends State<PlannerPage> {
     int length = c.getLength();
     for (int i = 0; i < length; ++i) {
       if (c.drives[i].serialNumber == d.serialNumber) {
-        if (c.manageMedia(backupToDisk.substring(0, 2), true)) {
-          showDialog(
-              context: context,
-              builder: (BuildContext context) {
-                return const AlertDialog(
-                  content: Text("Success load"),
-                );
-              });
-          //c.copyDir(diskToBackup, backupToDisk);
-          if (c.manageMedia(backupToDisk.substring(0, 2), false)) {
-            showDialog(
-                context: context,
-                builder: (BuildContext context) {
-                  return const AlertDialog(
-                    content: Text("Success eject"),
-                  );
-                });
-            return 1;
-          } else {
-            showDialog(
-                context: context,
-                builder: (BuildContext context) {
-                  return const AlertDialog(
-                    content: Text("Fail eject"),
-                  );
-                });
-            return 0;
-          }
-        } else {
-          showDialog(
-<<<<<<< HEAD
-              context: context,
-              builder: (BuildContext context) {
-                return const AlertDialog(
-                  content: Text("Fail load"),
-                );
-              });
-          return 0;
-=======
-            context: context, 
-            builder: (BuildContext context){
-              return const AlertDialog(
-                content: Text("Fail load"),
-              );
-            }
-          );
-        }
-        File('data\\flutter_assets\\assets\\scripts\\config.ini').writeAsStringSync('way=$diskToBackup\ndisk=$diskToBackup\nmytime=${time.hour}:${time.minute}\nscript=${Directory.current.path}\\data\\flutter_assets\\assets\\scripts\\Tasksch.bat');
-        File('data\\flutter_assets\\assets\\scripts\\Backup.bat').writeAsStringSync('');
-        Process.run('${Directory.current.path}\\data\\flutter_assets\\assets\\scripts\\Backup.bat', []);
-        if(c.manageMedia(backupToDisk.substring(0, 2), false)){
-          showDialog(
-            context: context, 
-            builder: (BuildContext context){
-              return const AlertDialog(
-                content: Text("Success eject"),
-              );
-            }
-          );
-        }
-        else{
-          showDialog(
-            context: context, 
-            builder: (BuildContext context){
-              return const AlertDialog(
-                content: Text("Fail eject"),
-              );
-            }
-          );
->>>>>>> f0bc0edea2181843a4955394dbae85d06cc235b5
-        }
+        return 1;
       }
     }
     return 0;
+  }
+
+  void media(bool signal){
+    if(signal == true){
+      if (c.manageMedia(backupToDisk.substring(0, 2), true)) {
+        showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return const AlertDialog(
+              content: Text("Success load"),
+            );
+          }
+        );
+      } 
+      else {
+        showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return const AlertDialog(
+              content: Text("Fail load"),
+            );
+          }
+        );      
+      }
+    }
+    if(signal == false){
+      if (c.manageMedia(backupToDisk.substring(0, 2), false)) {
+        showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return const AlertDialog(
+              content: Text("Success eject"),
+            );
+          }
+        );
+      } 
+      else {
+        showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return const AlertDialog(
+              content: Text("Fail eject"),
+            );
+          }
+        );
+      }
+    }
+  }
+
+  void writeFiles(){
+    File('data\\flutter_assets\\assets\\scripts\\config.ini').writeAsStringSync('way=$diskToBackup\ndisk=$diskToBackup\nmytime=${time.hour}:${time.minute}\nscript=${Directory.current.path}\\data\\flutter_assets\\assets\\scripts\\Tasksch.bat');
+    File('data\\flutter_assets\\assets\\scripts\\Backup.bat').writeAsStringSync('@rem Event\n@echo off\nchcp 850\nfor /f "tokens=1,2 delims==" %%a in (${Directory.current.path}\\data\\flutter_assets\\assets\\scripts\\config.ini) do (\n    if %%a==mytime set mytime=%%b\n    if %%a==day set day=%%b\n)\nschtasks /create /sc weekly /d %day% /tn hardtime /sd %date:~-10% /st %mytime% /tr ${Directory.current.path}\\data\\flutter_assets\\assets\\scripts\\Backup.bat');
+    File('data\\flutter_assets\\assets\\scripts\\Backup.bat').writeAsStringSync('start \\b ${Directory.current.path}\\data\\flutter_assets\\assets\\scripts\\load.exe \\\\.\\${backupToDisk.substring(0, 2)}\nset mydate=%DATE:~3,2%-%DATE:~0,2%-%DATE:~6,4%\nxcopy /y /o /e /d:%mydate% "$diskToBackup" "$backupToDisk"\nstart \\b ${Directory.current.path}\\data\\flutter_assets\\assets\\scripts\\eject.exe \\\\.\\${backupToDisk.substring(0, 2)}');
+    /*File('assets\\scripts\\config.ini').writeAsStringSync('way=$diskToBackup\ndisk=$backupToDisk\nday=$dayToBackup\nmytime=${time.hour}:${time.minute}\n');
+    File('assets\\scripts\\Tasksch.bat').writeAsStringSync('@rem Event\n@echo off\nchcp 850\nfor /f "tokens=1,2 delims==" %%a in (${Directory.current.path}\\assets\\scripts\\config.ini) do (\n    if %%a==mytime set mytime=%%b\n    if %%a==day set day=%%b\n)\nschtasks /create /sc weekly /d %day% /tn hardtime /sd %date:~-10% /st %mytime% /tr ${Directory.current.path}\\assets\\scripts\\Backup.bat');
+    File('assets\\scripts\\Backup.bat').writeAsStringSync('start \\b ${Directory.current.path}\\assets\\scripts\\load.exe \\\\.\\${backupToDisk.substring(0, 2)}\nset mydate=%DATE:~3,2%-%DATE:~0,2%-%DATE:~6,4%\nxcopy /y /o /e /d:%mydate% "$diskToBackup" "$backupToDisk"\nstart \\b ${Directory.current.path}\\assets\\scripts\\eject.exe \\\\.\\${backupToDisk.substring(0, 2)}');*/
   }
 }
